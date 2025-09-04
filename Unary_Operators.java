32746d6f0c7adaa195090a8f420b5162529c49b7
aniketsutar63 <sutaraniket63@gmail.com>
2025-09-04 23:33:29 +0530

    add

---
+++ Unary_Operators.java
@@ -0,0 +1,19 @@
+package operater;
+
+public class Unary_Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Interger declared
+        int a = 10;
+        int b = 10;
+
+        // Using unary operators
+        System.out.println("Postincrement : " + (a++));
+        System.out.println("Preincrement : " + (++a));
+
+        System.out.println("Postdecrement : " + (b--));
+        System.out.println("Predecrement : " + (--b));
+	}
+
+}
