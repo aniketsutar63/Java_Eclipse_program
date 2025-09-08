4b90f92a17571eb07a30fff0887b557833a1f31f
aniketsutar63 <sutaraniket63@gmail.com>
2025-09-08 12:26:29 +0530

    add

---
+++ Relational.java
@@ -0,0 +1,23 @@
+package operater;
+
+public class Relational {
+
+	public static void main(String[] args) {
+
+		int a=12;
+		int b=5;
+		int c=3;
+		
+		System.out.println("a == b"+(a == b));
+		System.out.println("a < b"+(b < a));
+		System.out.println("a <= b"+(b <= a));
+		System.out.println("a > b"+(b > a));
+		System.out.println("a >= b"+(b >= a));
+		
+		
+		
+		
+		
+	}
+
+}
